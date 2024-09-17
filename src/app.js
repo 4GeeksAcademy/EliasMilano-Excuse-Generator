dead1be96f6a06ec760571670ba6fcd5b5a7d87c
Elías Milano <87205729+MilanoTester@users.noreply.github.com>
2024-09-17 03:25:24 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,49 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = [
+    "Ronald McDonald",
+    "Uruguayan Spiderman",
+    "a homeless man",
+    "the policeman of Village People",
+    "a Disco Stu guy"
+  ];
+  let action = [
+    "took a hell of a bite of",
+    "ran away with",
+    "made a graffiti on",
+    "kicked",
+    "peed on"
+  ];
+  let what = [
+    "my report",
+    "an angry man",
+    "my sandwich",
+    "an old lady",
+    "my shoes"
+  ];
+  let when = [
+    "while I was in the toilete",
+    "while I was buying my lunch",
+    "when I was on my way to the office",
+    "while I was taking my daily nap",
+    "while he was hanging over the street"
+  ];
+
+  function random(array) {
+    let randomNumber = Math.floor(Math.random() * array.length);
+    return array[randomNumber];
+  }
+
+  let excuse = `
+    Sorry boss, 
+    ${random(who)} 
+    ${random(action)} 
+    ${random(what)} 
+    ${random(when)}`;
+
+  console.log(excuse);
+  document.getElementById("excuse").innerHTML = excuse;
 };
